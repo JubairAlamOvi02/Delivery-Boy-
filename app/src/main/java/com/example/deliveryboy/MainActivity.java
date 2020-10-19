4747b0dc3d1dc87ea26e744e40f8a3da25bd6a90
Oviex <mad07.work@gmail.com>
2020-10-19 12:11:55 +0600

    Reg

---
+++ app/src/main/java/com/example/deliveryboy/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.deliveryboy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(7000);
+                }
+                catch (Exception e ){
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent welcomeintent=new Intent(MainActivity.this,WelcomeActivity.class);
+                    startActivity(welcomeintent);
+
+
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
